db23154e1767be1984de46f8a305bdacdcb2b8ca
Ankur071 <ankuryadav7559@gmail.com>
2025-11-03 19:41:27 +0530

    Added routing for the components

--- src/app/layout/header-actions/header-actions.ts
+++ src/app/layout/header-actions/header-actions.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-actions',
-  imports: [],
+  imports: [MatButton, MatIconButton, MatIcon, RouterLink],
   template: `
-    <p>
-      header-actions works!
-    </p>
+    <div class="flex items-center gap-2">
+      <button matIconButton routerLink="/wishlist">
+        <mat-icon>favorites</mat-icon>
+      </button>
+      <button matIconButton>
+        <mat-icon>shopping_cart</mat-icon>
+      </button>
+      <button matButton>Sign In</button>
+      <button matButton="filled">Sign Up</button>
+      
+    </div>
   `,
   styles: ``,
 })
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar'
+import { HeaderActions } from "../header-actions/header-actions";
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar],
+  imports: [MatToolbar, HeaderActions],
   template: `
     <mat-toolbar class="w-full elevated py-2">
-      <div class="max-w-[1200px] mx-auto w-full">
+      <div class="max-w-[1200px] mx-auto w-full flex justify-between items-center">
+      <span>
       Modern Store
+      </span>
+      <app-header-actions />
       </div>
+      
     </mat-toolbar>
       
   `,
